98a9bdc3e8d8ab24c2743185d801551bb28c6a3b
Dan Ngo <danngo.au@gmail.com>
2025-03-12 18:09:19 +1100

    bootstrap installed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p class="text-primary">.text-primary</p>
+    <p class="text-secondary">.text-secondary</p>
+    <p class="text-success">.text-success</p>
+    <p class="text-danger">.text-danger</p>
+    <p class="text-warning">.text-warning</p>
+    <p class="text-info">.text-info</p>
+    <p class="text-light bg-dark">.text-light</p>
+    <p class="text-dark">.text-dark</p>
+    <p class="text-muted">.text-muted</p>
+    <p class="text-white bg-dark">.text-white</p>
+    <p class=""></p>
   </div>
 </template>
 
