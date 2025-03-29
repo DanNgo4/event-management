8d59091fd1973fab95ad67e0f7dfcf1c9b662097
Dan Ngo <danngo.au@gmail.com>
2025-03-29 18:07:43 +1100

    nav bar responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,76 @@
 <script setup>
+import { ref } from "vue";
 import { RouterLink } from "vue-router";
+
+import TieredMenu from "primevue/tieredmenu";
+
+const menuRef = ref(null);
+
+const pages = [
+  { label: "Home",              route: "/"     },
+  { label: "Event Information", route: "/info" },
+  { label: "Registration Form", route: "/form" }
+];
 </script>
 
 <template>
   <section>
     <header class="d-flex flex-row align-items-center justify-content-between px-5 py-2 bg-primary-subtle">
-      <a
-        class="fs-4 fw-bold bg-transparent cursor-pointer"
-        id="text-logo"
-        href="/"
-      >
+      <a class="fs-4 fw-bold bg-transparent cursor-pointer" href="/">
         Event Management System
       </a>
 
-      <nav class="d-flex gap-3 align-items-start">
-        <RouterLink to="/">Home</RouterLink>
+      <nav class="d-none d-lg-flex gap-3 align-items-center">
+        <RouterLink to="/">    Home             </RouterLink>
         <RouterLink to="/info">Event Information</RouterLink>
         <RouterLink to="/form">Registration Form</RouterLink>
       </nav>
+
+      <div class="d-flex d-lg-none align-items-center">
+        <button class="btn btn-link p-0" @click="menuRef.toggle($event)">
+          <i class="fas fa-bars fs-3"></i>
+        </button>
+
+        <TieredMenu
+          :model="pages"
+          popup
+          ref="menuRef"
+          class="custom-tiered-menu"
+        >
+          <template #item="{ item }">
+            <RouterLink
+              :to="item.route"
+              custom
+              v-slot="{ href }"
+            >
+              <a v-ripple :href="href">
+                <span :class="item.icon"></span>
+
+                <p>{{ item.label }}</p>
+              </a>
+            </RouterLink>
+          </template>
+        </TieredMenu>
+      </div>
     </header>
   </section>
 </template>
 
 <style scoped>
-  #text-logo {
-    cursor: pointer;
+  .custom-tiered-menu {
+    background-color: rgb(7, 27, 46);
+    border: 1px solid #ddd;
+    border-radius: 0.25rem;
+  }
+
+  .custom-tiered-menu a {
+    color: white;
+    padding: 0.75rem 1.5rem;
+    display: flex;
+    align-items: center;
+  }
+
+  .custom-tiered-menu a:hover {
+    background-color: rgba(255, 255, 255, 0.1);
   }
 </style>
