bc379ffa388691f4e29a98d035da08fb6ca7fa0d
Dan Ngo <danngo.au@gmail.com>
2025-03-29 18:28:12 +1100

    refine home view responsive layout

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,6 +1,3 @@
-<script setup>
-</script>
-
 <template>
   <article class="d-flex flex-column justify-content-center align-items-center text-center">
     <img src="./swinburne_logo.png" alt="Swinburne Logo" class="my-4" />
@@ -9,45 +6,43 @@
 
     <section class="container my-4">
       <div class="row g-4 mb-4">
-        <div class="col-lg-4 col-md-12 col-sm-12">
-          <div class="card p-5 shadow-sm">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="card custom-card shadow-sm flex-column p-3">
             <h4>Efficient Event Planning</h4>
             <p>We handle everything from start to finish, ensuring seamless execution.</p>
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-12 col-sm-12">
-          <div class="card p-5 shadow-sm">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="card custom-card shadow-sm flex-column p-3 border-1">
             <h4>Expert Team</h4>
             <p>Our professionals bring years of experience to create successful events.</p>
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-12 col-sm-12">
-          <div class="card p-5 shadow-sm">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="card custom-card shadow-sm flex-column p-3 border-1">
             <h4>Cost-Effective Solutions</h4>
             <p>We provide budget-friendly events management.</p>
           </div>
         </div>
-      </div>
 
-      <div class="row g-4">
-        <div class="col-lg-4 col-md-12 col-sm-12">
-          <div class="card p-5 shadow-sm">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="card custom-card shadow-sm flex-column p-3 border-1">
             <h4>On-Time Execution</h4>
             <p>Timely delivery is our priority to ensure a hassle-free experience.</p>
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-12 col-sm-12">
-          <div class="card p-5 shadow-sm">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="card custom-card shadow-sm flex-column p-3 border-1">
             <h4>High Customer Satisfaction</h4>
             <p>Our clients trust us for top-notch events experiences and services.</p>
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-12 col-sm-12">
-          <div class="card p-5 shadow-sm">
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <div class="card custom-card shadow-sm flex-column p-3 border-1">
             <h4>Strong Communication</h4>
             <p>We keep you informed throughout the process for a smooth experience</p>
           </div>
@@ -58,7 +53,7 @@
 </template>
 
 <style scoped>
-  /* .card {
-    height: 7.5rem;
-  } */
+  .custom-card {
+    height: 8rem;
+  }
 </style>
